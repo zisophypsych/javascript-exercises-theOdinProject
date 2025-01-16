8174fb2d35a2ae7214052bfbf88561cfbe9eea9d
Ziyaul Haq <ziyaulhaqkhan60@gmail.com>
2025-01-16 10:50:58 +0530

    Find Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    if (a < 0){
+        return 'OOPS'
+    } else{
+        series = [0,1];
+        for(let i = 2; i <= a; i++){
+        fibo = series[i-1] + series[i-2];
+        series.push(fibo);
+        }
+        return series[a];
 
+    }
+    
 };
 
+// console.log(fibonacci(1))
 // Do not edit below this line
 module.exports = fibonacci;
