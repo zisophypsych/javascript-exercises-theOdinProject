aa3f38604a70ee0467f28ddb60a04d57b2d3c930
Ziyaul Haq <ziyaulhaqkhan60@gmail.com>
2025-01-16 10:30:47 +0530

    Check Palindrome Function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (a) {
+    cleanedString = a.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    joined = cleanedString.split('').reverse().join('');
+    if (joined === cleanedString){
+        return true;
+    } else {
+        return false;
+    }
 };
-
 // Do not edit below this line
 module.exports = palindromes;
