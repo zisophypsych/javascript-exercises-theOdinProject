80b66e74e1944dde5d3a42dea2ce54398a0d894d
Ziyaul Haq <ziyaulhaqkhan60@gmail.com>
2025-01-16 11:55:32 +0530

    FindtheOldest Function Using sort() method

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -5,24 +5,5 @@ const findTheOldest = function(array) {
     return sortedArray[0];
 };
 
-const people = [
-    {
-      name: "Carly",
-      yearOfBirth: 1942,
-      yearOfDeath: 1970,
-    },
-    {
-      name: "Ray",
-      yearOfBirth: 1962,
-      yearOfDeath: 2011,
-    },
-    {
-      name: "Jane",
-      yearOfBirth: 1912,
-      yearOfDeath: 1941,
-    },
-  ]
-
-  console.log(findTheOldest(people))
 // Do not edit below this line
 module.exports = findTheOldest;
